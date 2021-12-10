cae46980a5484db7b45c85e10b550a17672f70a6
you@2.com <you@2.com>
2021-12-10 12:16:33 +0000

    Povoar.sqp

---
+++ Entrega2/SQL/povoar.sql
@@ -0,0 +1,108 @@
+insert into atleta values(1,'Tsanko Arnaudov','M','14-03-1992');
+insert into atleta values(2,'Marco Fortes','M','26-09-1982');
+insert into atleta values(3,'Francisco Belo','M','27-03-1991');
+insert into atleta values(4,'Fernando Alves','M','24-06-1967');
+insert into atleta values(5,'António Vital e Silva','M','23-01-1988');
+
+/*Peso*/
+
+insert into atleta values(6,'Fracis Obikwelu','M','22-11-1978');
+insert into atleta values(7,'David Lima','M','06-09-1990');
+insert into atleta values(8,'Carlos Calado','M','05-10-1975');
+insert into atleta values(9,'Carlos Nascimento','M','12-10-1994');
+insert into atleta values(10,'Yazaldes Nascimento','M','17-04-1986');
+insert into atleta values(11,'Lorene Dorcas Bazolo','F','22-11-1978');
+insert into atleta values(12,'Lucrécia Jardim','F','28-01-1971');
+insert into atleta values(13,'Sónia Tavares','F','21-03-1986');
+insert into atleta values(14,'Severina Cravid','F','01-07-1978');
+insert into atleta values(15,'Carla Tavares','F','22-05-1982');
+
+/*100m*/
+
+insert into atleta values(16,'William Reais','M','04-05-1999');
+insert into atleta values(17,'Arnaldo Abrantes','M','27-11-1986');
+insert into atleta values(18,'Diogo Antunes','M','02-11-1992');
+insert into atleta values(19,'Sandra Castanheira','F','08-11-1973');
+
+
+/*300m*/
+insert into atleta values(20,'Victor Ricardo Santos','M','18-12-1994');
+insert into atleta values(21,'Carlos Silva','M','08-06-1974');
+insert into atleta values(22,'Maria Carmo Tavares','F','27-04-1974');
+insert into atleta values(23,'Rivinilda Mentai','F','01-04-1994');
+
+/*400m*/
+insert into atleta values(24,'Raidel Acea','M','31-10-1990');
+insert into atleta values(25,'Cátia Azevedo','F','09-03-1994');
+
+insert into evento values(1,'M','4x100M');
+insert into evento values(2,'F','4x100M');
+insert into evento values(3,'M','4x400M');
+insert into evento values(4,'F','4x400M');
+insert into evento values(5,'M','60M');
+insert into evento values(6,'F','60M');
+insert into evento values(7,'M','60MB');
+insert into evento values(8,'F','60MB');
+insert into evento values(9,'M','100M');
+insert into evento values(10,'F','100M');
+insert into evento values(11,'M','100MB');
+insert into evento values(12,'F','100MB');
+insert into evento values(13,'M','110MB');
+insert into evento values(14,'F','110MB');
+insert into evento values(15,'M','200M');
+insert into evento values(16,'F','200M');
+insert into evento values(17,'M','300M');
+insert into evento values(18,'F','300M');
+insert into evento values(19,'M','300MB');
+insert into evento values(20,'F','300MB');
+insert into evento values(21,'M','400M');
+insert into evento values(22,'F','400M');
+insert into evento values(23,'M','400MB');
+insert into evento values(24,'F','400MB');
+insert into evento values(25,'M','800M');
+insert into evento values(26,'F','800M');
+insert into evento values(27,'M','1000M');
+insert into evento values(28,'F','1000M');
+insert into evento values(29,'M','1500M');
+insert into evento values(30,'F','1500M');
+insert into evento values(31,'M','3000M');
+insert into evento values(32,'F','3000M');
+insert into evento values(33,'M','3000MO');
+insert into evento values(34,'F','3000MO');
+insert into evento values(35,'M','5000M');
+insert into evento values(36,'F','5000M');
+insert into evento values(37,'M','10000M');
+insert into evento values(38,'F','10000M');
+insert into evento values(39,'M','20KmM');
+insert into evento values(40,'F','20KmM');
+insert into evento values(41,'M','50KmM');
+insert into evento values(42,'F','50KmM');
+insert into evento values(43,'M','MARATONA');
+insert into evento values(44,'F','MARATONA');
+insert into evento values(45,'M','COMPRIMENTO');
+insert into evento values(46,'F','COMPRIMENTO');
+insert into evento values(47,'M','ALTURA');
+insert into evento values(48,'F','ALTURA');
+insert into evento values(49,'M','VARA');
+insert into evento values(50,'F','VARA');
+insert into evento values(51,'M','TRIPLO');
+insert into evento values(52,'F','TRIPLO');
+insert into evento values(53,'M','PESO');
+insert into evento values(54,'F','PESO');
+insert into evento values(55,'M','DISCO');
+insert into evento values(56,'F','DISCO');
+insert into evento values(57,'M','DARDO');
+insert into evento values(58,'F','DARDO');
+insert into evento values(59,'M','MARTELO');
+insert into evento values(60,'F','MARTELO');
+insert into evento values(61,'M','DECATLO');
+insert into evento values(62,'F','DECATLO');
+insert into evento values(63,'M','HEPTATLO');
+insert into evento values(64,'F','HEPTATLO');
+insert into evento values(65,'M','PENTATLO');
+insert into evento values(66,'F','PENTATLO');
+insert into evento values(67,'M','TRIATLO');
+insert into evento values(68,'F','TRIATLO');
+
+
+
